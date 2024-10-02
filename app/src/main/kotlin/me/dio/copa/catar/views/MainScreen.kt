254f83a9cc8cf7e8f2f57812361d55d8d2d79a28
asafe alves lopes <asafelopesdev@outlook.com>
2024-10-02 11:54:10 -0300

    runtime permissions added

--- app/src/main/kotlin/me/dio/copa/catar/views/MainScreen.kt
+++ app/src/main/kotlin/me/dio/copa/catar/views/MainScreen.kt
@@ -1,5 +1,11 @@
 package me.dio.copa.catar.views
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -12,21 +18,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import coil.compose.AsyncImage
 import me.dio.copa.catar.R
 import me.dio.copa.catar.domain.extensions.getDate
@@ -115,14 +128,60 @@ fun SuccessScreen(
     matches: List<Match>,
     notificationClick: (match: Match, title: String, content: String) -> Unit
 ) {
-    Column {
+    val context = LocalContext.current
+    val bellMsg = stringResource(id = R.string.msg_notification_not_allowed)
+    val isPostNotificationPermissionNecessary =
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+
+    var notificationIsAllowed by remember {
+        mutableStateOf(
+            when {
+                isPostNotificationPermissionNecessary -> ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+
+                else -> true
+            }
+        )
+    }
+
+    val askPermission = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { allowed ->
+            notificationIsAllowed = allowed
+            if (!allowed)
+                Toast
+                    .makeText(context, bellMsg, Toast.LENGTH_LONG)
+                    .show()
+        }
+    )
+
+    Box {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(Sizes.defaultSpacing),
         ) {
             items(matches.size) { index ->
                 if (index == 0)
                     Spacer(modifier = Modifier.height(Sizes.defaultSpacing))
-                MatchCard(match = matches[index], notificationClick)
+                MatchCard(match = matches[index], notificationIsAllowed, notificationClick)
+            }
+        }
+        if (isPostNotificationPermissionNecessary && !notificationIsAllowed) {
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.BottomCenter)
+                    .background(Color.White)
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(Sizes.defaultSpacing),
+                    onClick = {
+                        askPermission.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    }) {
+                    Text(text = stringResource(id = R.string.btn_allow_notification))
+                }
             }
         }
     }
@@ -131,8 +190,11 @@ fun SuccessScreen(
 @Composable
 fun MatchCard(
     match: Match,
+    canNotify: Boolean,
     notificationClick: (match: Match, title: String, content: String) -> Unit
 ) {
+    val context = LocalContext.current
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -164,19 +226,25 @@ fun MatchCard(
                 else R.drawable.ic_notifications
                 val title = stringResource(id = R.string.notification_title)
                 val content = stringResource(id = R.string.notification_content)
+                val bellMsg = stringResource(id = R.string.msg_notification_not_allowed)
                 Image(
                     painter = painterResource(id = drawable),
                     modifier = Modifier
                         .padding(Sizes.defaultSpacing)
                         .clickable {
-                            notificationClick(
-                                match,
-                                title,
-                                content.replaceMarkToTeam(
-                                    match.team1.displayName,
-                                    match.team2.displayName
+                            if (canNotify)
+                                notificationClick(
+                                    match,
+                                    title,
+                                    content.replaceMarkToTeam(
+                                        match.team1.displayName,
+                                        match.team2.displayName
+                                    )
                                 )
-                            )
+                            else
+                                Toast
+                                    .makeText(context, bellMsg, Toast.LENGTH_LONG)
+                                    .show()
                         },
                     contentDescription = null,
                 )
